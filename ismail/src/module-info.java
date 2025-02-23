ddaa3729c5cfc13b2999805678a7f97491d6e35e
InshaManowar <inshamanowar22@gmail.com>
2025-02-23 11:03:24 +0530

    first commit

---
+++ ismail/src/module-info.java
@@ -0,0 +1,14 @@
+package ftgroup1.com.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+		System.out.print("Hello World");
+	}
+
+}
